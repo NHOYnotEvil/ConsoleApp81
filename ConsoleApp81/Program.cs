0a1bcbbaf115fec1d99e7fe94998f9ab25e55fef
NHOY <NHOY@2210DR000919>
2023-06-20 16:15:39 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp81/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace ConsoleApp81
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            string symbol;
+            int lengthName = 0;
+
+            Console.Write("Введите имя:");
+            name = Console.ReadLine();
+            Console.Write("Введите символ, который обведёт ваше имя в рамку:");
+            symbol = Console.ReadLine();
+            lengthName = name.Length + 2;
+
+            for (int i = 0; i < lengthName; i++)
+            {
+                Console.Write(symbol);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine(symbol + name + symbol);
+
+            for (int i = 0; i < lengthName; i++)
+            {
+                Console.Write(symbol);
+            }
+        }
+    }
+}
